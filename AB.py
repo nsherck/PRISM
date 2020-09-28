9625b5941f2a4578ca66de8aac17fe1fe0b0c627
Nick Sherck <nsherck@ucsb.edu>
2020-09-28 10:58:30 -0700

    minor modifications to clean up some

--- AB.py
+++ AB.py
@@ -1,16 +1,20 @@
+#import sys, os
+#cwd = os.getcwd()
+#sys.path.append(os.path.join(cwd,'pyPRISMpackage/pyPRISM'))
 import pyPRISM
+import pyPRISM
+print(pyPRISM.__file__)
+
 import numpy as np
 import matplotlib.pyplot as plt
-print(pyPRISM.__file__)
 import scipy as sp
 
 sigma = 1.
 scale = 1
 kbT = 1.
 dr = 0.01
-power = 10
-PY = False
-HNC = True
+power = 10 # 2**power == # grid points; therefore length = dr*2**power
+
 sys = pyPRISM.System(['A'],kT=kbT)
 sys.domain = pyPRISM.Domain(dr=dr,length=2**power)
 print('r =',sys.domain.r)
@@ -46,13 +50,14 @@ for _i, xfact in enumerate(scales):
     _iter = 0
     Norm = 1
     alpha = 0.9
-    while Norm > 1e-4:
+    while Norm > 1e-4: # custom update 
         if _iter == 0:
             gamma_in = guess_1
         out = PRISM.cost(gamma_in)
         print(out[0].shape) # GammaOut
-        print(out[1].shape) # GammaIn
-
+        print(out[1].shape) # GammaIng
+        
+        # update gamma
         gamma_new = np.add(alpha*out[1],(1-alpha)*out[0])
         #gamma_new = np.multiply(sys.domain.r,gamma_new)
         print(gamma_new.shape)
@@ -72,15 +77,12 @@ for _i, xfact in enumerate(scales):
 
     x = sys.domain.r
     RDF_AA = pyPRISM.calculate.pair_correlation(PRISM)['A','A']
-    #RDF_AB = pyPRISM.calculate.pair_correlation(PRISM)['A','B']
     np.savetxt('RDF_AA.dat',np.column_stack((x,RDF_AA)))
-    #np.savetxt('RDF_AB.dat',np.column_stack((x,RDF_AB)))
     data = np.loadtxt('A_RDF.txt',delimiter=',')
     data1 = np.loadtxt('RDF_POL_POL_uab_0.0.dat')
     plt.plot(data[:,0],data[:,1],'or')
     plt.plot(data1[:,0]*10,data1[:,1],'ob')
     plt.plot(x, RDF_AA, "-r")
-    #plt.plot(x, RDF_AB, "-b")
     plt.xlim(0,10)
     plt.savefig('RDF.png')
     plt.show()
@@ -126,79 +128,79 @@ matrix_in = matrix_in.flatten()
 #print(guess_chk.shape)
 
 
-if HNC:
-    guess_1 = np.zeros(sys.rank*sys.rank*sys.domain.length)
-    #xfrac_list = [0]
-    #xfrac_list.extend(np.linspace(0.1,1,10))
+guess_1 = np.zeros(sys.rank*sys.rank*sys.domain.length)
+#xfrac_list = [0]
+#xfrac_list.extend(np.linspace(0.1,1,10))
+
+for _i,_xfrac in enumerate([1.]):
+    print('Using HNC closure. Density: ')
+    sys.density['A']       = 0.5
+    sys.density['B']       = 0.5
     
-    for _i,_xfrac in enumerate([1.]):
-        print('Using HNC closure. Density: ')
-        sys.density['A']       = 0.5
-        sys.density['B']       = 0.5
-        
-        sys.kT = 1
-        sys.closure['A','A'] = pyPRISM.closure.HyperNettedChain(apply_hard_core=False)
-        sys.closure['A','B'] = pyPRISM.closure.HyperNettedChain(apply_hard_core=False)
-        sys.closure['B','B'] = pyPRISM.closure.HyperNettedChain(apply_hard_core=False)
-        
-        _xfrac = 1.
-        print('Using HNC closure. Density: {}'.format(sys.density))
-        uAB = (7.77817-uAA)*_xfrac + uAA
-        print('uAB {} and uAA {}'.format(uAB,uAA))
-        sys.potential['A','B'] = pyPRISM.potential.Gaussian(epsilon=-1*uAB,alpha=range,sigma=0,high_value=uAB)
-        PRISM = sys.createPRISM()
-        
-        _iter = 0
-        Norm = 1
-        alpha = 0.99
-        while Norm > 0.01:
-            if _iter == 0:
-                gamma_in = guess_1
-            out = PRISM.cost(gamma_in)
-            print(out[0].shape) # GammaOut
-            print(out[1].shape) # GammaIn
+    sys.kT = 1
+    sys.closure['A','A'] = pyPRISM.closure.HyperNettedChain(apply_hard_core=False)
+    sys.closure['A','B'] = pyPRISM.closure.HyperNettedChain(apply_hard_core=False)
+    sys.closure['B','B'] = pyPRISM.closure.HyperNettedChain(apply_hard_core=False)
+    
+    _xfrac = 1.
+    print('Using HNC closure. Density: {}'.format(sys.density))
+    uAB = (7.77817-uAA)*_xfrac + uAA
+    print('uAB {} and uAA {}'.format(uAB,uAA))
+    sys.potential['A','B'] = pyPRISM.potential.Gaussian(epsilon=-1*uAB,alpha=range,sigma=0,high_value=uAB)
+    PRISM = sys.createPRISM()
+    
+    _iter = 0
+    Norm = 1
+    alpha = 0.99
+    while Norm > 0.01:
+        if _iter == 0:
+            gamma_in = guess_1 # tried playing around with seed based off one component
+        out = PRISM.cost(gamma_in)
+        print(out[0].shape) # GammaOut
+        print(out[1].shape) # GammaIn
 
-            gamma_new = np.add(alpha*out[1],(1-alpha)*out[0])
-            #gamma_new = np.multiply(sys.domain.r,gamma_new)
-            print(gamma_new.shape)
-            Norm = cust_norm(out[1],out[0])
-            
-            #plt.plot(sys.domain.r,gamma_in[0:int(2**power)],'-r')
-            #plt.plot(sys.domain.r,gamma_new[0:int(2**power)],'-b')
-            #plt.show()
-            #plt.close()
-            gamma_in = np.copy(gamma_new)
-            print('Iter: {} Norm: {}'.format(_iter,Norm))
-            _iter += 1
+        gamma_new = np.add(alpha*out[1],(1-alpha)*out[0])
+        #gamma_new = np.multiply(sys.domain.r,gamma_new)
+        print(gamma_new.shape)
+        Norm = cust_norm(out[1],out[0])
         
+        #plt.plot(sys.domain.r,gamma_in[0:int(2**power)],'-r')
+        #plt.plot(sys.domain.r,gamma_new[0:int(2**power)],'-b')
+        #plt.show()
+        #plt.close()
+        gamma_in = np.copy(gamma_new)
+        print('Iter: {} Norm: {}'.format(_iter,Norm))
+        _iter += 1
+    
 
-        #dirCorr = PRISM.directCorr()
-        
-        
-        #PRISM.solve(guess=guess_1,method='anderson',options={'maxiter':1000,'jac_options':{'M':5},'tol_norm':cust_norm})#guess=matrix_in,method='Krylov')#,options={'maxiter':500})
-        
-        out = PRISM.cost(guess_1)
-        print(out[0].shape)
-        print(out[1].shape)
-        guess_1 = np.copy(PRISM.x)
+    #dirCorr = PRISM.directCorr()
+    
+    
+    #PRISM.solve(guess=guess_1,method='anderson',options={'maxiter':1000,'jac_options':{'M':5},'tol_norm':cust_norm})#guess=matrix_in,method='Krylov')#,options={'maxiter':500})
+    
+    out = PRISM.cost(guess_1)
+    print(out[0].shape)
+    print(out[1].shape)
+    guess_1 = np.copy(PRISM.x)
 
-        x = sys.domain.r
-        RDF_AA = pyPRISM.calculate.pair_correlation(PRISM)['A','A']
-        RDF_AB = pyPRISM.calculate.pair_correlation(PRISM)['A','B']
-        np.savetxt('RDF_AA.dat',np.column_stack((x,RDF_AA)))
-        np.savetxt('RDF_AB.dat',np.column_stack((x,RDF_AB)))
-        data = np.loadtxt('A_RDF.txt',delimiter=',')
-        data1 = np.loadtxt('RDF_POL_POL_uaa_2.0.dat')
-        plt.plot(data[:,0],data[:,1],'or')
-        plt.plot(data1[:,0]*10,data1[:,1],'ob')
-        plt.plot(x, RDF_AA, "-r")
-        plt.plot(x, RDF_AB, "-b")
-        plt.xlim(0,10)
-        plt.savefig('RDF.png')
-        plt.show()
-        plt.close()
+    x = sys.domain.r
+    RDF_AA = pyPRISM.calculate.pair_correlation(PRISM)['A','A']
+    RDF_AB = pyPRISM.calculate.pair_correlation(PRISM)['A','B']
+    np.savetxt('RDF_AA.dat',np.column_stack((x,RDF_AA)))
+    np.savetxt('RDF_AB.dat',np.column_stack((x,RDF_AB)))
+    data = np.loadtxt('A_RDF.txt',delimiter=',')
+    data1 = np.loadtxt('RDF_POL_POL_uaa_2.0.dat')
+    plt.plot(data[:,0],data[:,1],'or')
+    plt.plot(data1[:,0]*10,data1[:,1],'ob')
+    plt.plot(x, RDF_AA, "-r")
+    plt.plot(x, RDF_AB, "-b")
+    plt.xlim(0,10)
+    plt.savefig('RDF.png')
+    plt.show()
+    plt.close()
 
 ''' calculate pressure '''
+# Left over from doing just single species
 xsq = np.multiply(x,x)
 xsqsq = np.multiply(xsq,xsq)
 integrand = 8*np.pi*range*uAB*np.multiply(np.multiply(xsqsq,np.exp(-1*range*xsq)),RDF_AA)
